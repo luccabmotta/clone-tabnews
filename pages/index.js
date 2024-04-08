dfda4f6deb3bdcf9af960f575b364ac88899aa55
Lucca Motta <53232877+luccabmotta@users.noreply.github.com>
2024-04-08 02:26:42 +0000

    Nova mensagem de apoio

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Bem vindo novamente </h1>
+    return <h1> Bem vindo, jovem padawan. O aprendizado é lento, o processo é demorado. A paciência é necessária, mas a cada dia, você ficará mais forte...acredite em si mesmo sempreeee! </h1>
 }
 
 export default Home;
