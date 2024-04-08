7a28c046403781fa0575cb9a35799428f2e4152c
Lucca Motta <53232877+luccabmotta@users.noreply.github.com>
2024-04-08 02:35:38 +0000

    Mensagem de boa noite

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Bem vindo, jovem padawan. O aprendizado é lento, o processo é demorado. A paciência é necessária, mas a cada dia, você ficará mais forte...acredite em si mesmo sempreeee! </h1>
+    return <h1> Boa noite Kawane, durma bem! </h1>
 }
 
 export default Home;
